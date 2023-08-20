6bdc175244c2b29c7d19806f3b198166f9f098d5
RyoSakon001 <ryo.development0320@gmail.com>
2023-08-20 12:45:54 +0900

    test

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -4,7 +4,7 @@ const TodoList = () => {
   return (
     <ul className='space-y-3'>
       <li className='flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow'>
-        <span>散歩</span>
+        <span>散歩をする</span>
       <div>
         <button className='text-green-500 mr-3'>edit</button>
         <button className='text-red-500'>delete</button>
